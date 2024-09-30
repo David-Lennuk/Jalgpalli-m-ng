3202fed4bf92fde216b54cc15b45eeb4934c6f93
David-Lennuk <153904926+David-Lennuk@users.noreply.github.com>
2024-09-30 16:44:38 +0300

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -1,58 +1,51 @@
-namespace Jalgpalli;
-
-public class Game
+namespace Jalgpalli
 {
-    public Team HomeTeam { get; }
-    public Team AwayTeam { get; }
-    public Stadium Stadium { get; }
-    public Ball Ball { get; private set; }
+    public class Game
+    {
+        public Team HomeTeam { get; }
+        public Team AwayTeam { get; }
+        public Stadium Stadium { get; }
+        public Ball Ball { get; private set; }
 
-    public Game(Team homeTeam, Team awayTeam, Stadium stadium)
-    {
-        HomeTeam = homeTeam;
-        homeTeam.Game = this;
-        AwayTeam = awayTeam;
-        awayTeam.Game = this;
-        Stadium = stadium;
-    }
-
-    public void Start() // Создаём мяч и комманды
-    {
-        Ball = new Ball(Stadium.Width / 2, Stadium.Height / 2, this);
-        HomeTeam.StartGame(Stadium.Width / 2, Stadium.Height);
-        AwayTeam.StartGame(Stadium.Width / 2, Stadium.Height);
-    }
-    private (double, double) GetPositionForAwayTeam(double x, double y) // Получение позиции для комманды Away
-    {
-        return (Stadium.Width - x, Stadium.Height - y);
-    }
-
-    public (double, double) GetPositionForTeam(Team team, double x, double y) // Получение позиции для комманды
-    {
-        return team == HomeTeam ? (x, y) : GetPositionForAwayTeam(x, y);
-    }
-
-    public (double, double) GetBallPositionForTeam(Team team) // Получение позиции мяча для комманды
-    {
-        return GetPositionForTeam(team, Ball.X, Ball.Y);
-    }
-
-    public void SetBallSpeedForTeam(Team team, double vx, double vy) // Установка скорости мяча для комманды
-    {
-        if (team == HomeTeam)
+        public Game(Team homeTeam, Team awayTeam, Stadium stadium)
         {
-            Ball.SetSpeed(vx, vy);
+            HomeTeam = homeTeam;
+            AwayTeam = awayTeam;
+            Stadium = stadium;
         }
-        else
-        {
-            Ball.SetSpeed(-vx, -vy);
-        }
-    }
 
-    public void Move() // Ввести всё в движение
-    {
-        HomeTeam.Move();
-        AwayTeam.Move();
-        Ball.Move();
+        public void Start()
+        {
+            Ball = new Ball(Stadium.Width / 2, Stadium.Height / 2, this);
+            HomeTeam.StartGame(Stadium.Width, Stadium.Height);
+            AwayTeam.StartGame(Stadium.Width, Stadium.Height);
+        }
+
+        private void CheckForGoals()
+        {
+            if (Ball.X < 0) // Левые ворота
+            {
+                AwayTeam.AddScore();
+                ResetBall();
+            }
+            else if (Ball.X >= Stadium.Width) // Правые ворота
+            {
+                HomeTeam.AddScore();
+                ResetBall();
+            }
+        }
+
+        private void ResetBall()
+        {
+            Ball = new Ball(Stadium.Width / 2, Stadium.Height / 2, this);
+        }
+
+        public void Move()
+        {
+            HomeTeam.Move();
+            AwayTeam.Move();
+            Ball.Move();
+            CheckForGoals();
+        }
     }
 }
