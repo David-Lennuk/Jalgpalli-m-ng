38af500aaab0f9a057759e70710f6b96ad621ff8
David-Lennuk <153904926+David-Lennuk@users.noreply.github.com>
2024-10-14 02:16:47 +0300

    Update Ball.cs

--- Ball.cs
+++ Ball.cs
@@ -1,17 +1,25 @@
 using System;
 
-namespace Jalgpalli
+namespace Jalgpali
 {
     public class Ball
     {
         public double X { get; private set; }
         public double Y { get; private set; }
+
         private double _vx, _vy;
+
         private Game _game;
 
         public Ball(double x, double y, Game game)
         {
             _game = game;
+            X = x;
+            Y = y;
+        }
+
+        public void SetPosition(double x, double y)
+        {
             X = x;
             Y = y;
         }
@@ -27,17 +35,28 @@ namespace Jalgpalli
             double newX = X + _vx;
             double newY = Y + _vy;
 
-            // Проверяем столкновение с границами
-            if (newX < 0 || newX >= _game.Stadium.Width)
+            if (_game.Stadium.IsIn(newX, newY))//проверка на столкновение с стеной
             {
-                newX = _game.Stadium.Width / 2; // Сбрасываем на центр
-                newY = _game.Stadium.Height / 2; // Сбрасываем на центр
-                _vx = _vy = 0; // Останавливаем мяч
+                X = newX;
+                Y = newY;
             }
+            else
+            {
 
-            // Обновляем положение мяча
-            X = newX;
-            Y = newY;
+                if (newX < 0 || newX >= _game.Stadium.Width)//рикошет от стены
+                {
+                    _vx = -_vx; //смена направление по оси X
+                    newX = X + _vx; //перемещение мяч на 1 пиксель
+                    X = newX < 0 ? 1 : newX >= _game.Stadium.Width ? _game.Stadium.Width - 1 : newX;
+                }
+
+                if (newY < 0 || newY >= _game.Stadium.Height)
+                {
+                    _vy = -_vy; //сменна направление по оси Y
+                    newY = Y + _vy; //перимещаем мяч на 1 пиксель
+                    Y = newY < 0 ? 1 : newY >= _game.Stadium.Height ? _game.Stadium.Height - 1 : newY;
+                }
+            }
         }
     }
 }
